62ed4534661719f14c545710b3fb9ee05a6d5570
Shutirtha Roy <shutirtha.roy1@gmail.com>
2022-06-01 13:17:50 +0600

    Initial Commit

---
+++ MagicDestroyers/MagicDestroyers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagicDestroyers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
